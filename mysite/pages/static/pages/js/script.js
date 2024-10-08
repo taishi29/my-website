15ed2fa1adf5850eb11fda41e4d468369f754c9c
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-10-08 20:18:57 +0900

    トップページの下に、selectionを追加しました。また、要素が特定の範囲内にスクロールされたときにアニメーションが実行されるように、JavaScript(jQuery)によって実装しました。

---
+++ mysite/pages/static/pages/js/script.js
@@ -0,0 +1,21 @@
+// scroll_effect
+$(document).ready(function () {
+    // 要素の取得
+    var scrollAnimationElm = document.querySelectorAll('.scroll_up, .scroll_left, .scroll_right');
+
+    // アニメーションを実行する関数
+    var scrollAnimationFunc = function () {
+        for (var i = 0; i < scrollAnimationElm.length; i++) {
+            var triggerMargin = 100;
+            if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+                scrollAnimationElm[i].classList.add('on');
+            }
+        }
+    };
+
+    // ページが読み込まれた時点でアニメーションを実行
+    scrollAnimationFunc();
+
+    // スクロール時にもアニメーションを実行
+    $(window).on('scroll', scrollAnimationFunc);
+});
