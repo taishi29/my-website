3ae1757ff32b7559cb41b4ba5d70ebeb3adf9992
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-10-10 23:35:44 +0900

    アニメーションの修正を行いました。

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s(@ztlxrm_a-&h9o+lu(4wi=wxr*#7xk$)u#!@@ycfy!e_0fxa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-websitegunicorn-your-project-name-wsgi.onrender.com', 'loalhost']
+ALLOWED_HOSTS = []
 
 
 
