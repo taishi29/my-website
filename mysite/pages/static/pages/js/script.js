8f0d0a12f38e429cdf9226bc66af053e5244ad60
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-10-11 01:56:20 +0900

    上にスクロールしたらアニメーションが戻るように修正しました。

--- mysite/pages/static/pages/js/script.js
+++ mysite/pages/static/pages/js/script.js
@@ -1,4 +1,6 @@
-// scroll_effect
+/* scroll_effect */
+// getBoundingClientRect().top：要素の上端がビューポートの上端からどれくらいの距離にあるか。
+// window.innerHeight：ビューポートの高さを取得。
 $(document).ready(function () {
     // 要素の取得
     var scrollAnimationElm = document.querySelectorAll('.scroll_up, .scroll_left, .scroll_right, .animate');
@@ -19,3 +21,37 @@ $(document).ready(function () {
     // スクロール時にもアニメーションを実行
     $(window).on('scroll', scrollAnimationFunc);
 });
+
+
+$(document).ready(function () {
+    // 要素の取得
+    var scrollAnimationElm = document.querySelectorAll('.extinction, .page-title');
+    var englishMessageElm = $('.welcome');
+
+    var scrollAnimationFunc = function () {
+        for (var i = 0; i < scrollAnimationElm.length; i++) {
+            var triggerMargin = 625;
+
+            // 要素がビューポート内に入ったときの処理
+            if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+                scrollAnimationElm[i].classList.add('on');
+
+                // 英語のメッセージを表示
+                englishMessageElm.removeClass('hidden').addClass('visible');
+            } else {
+                // 要素がビューポート外に出たときの処理
+                scrollAnimationElm[i].classList.remove('on');
+
+                // 英語のメッセージを非表示
+                englishMessageElm.removeClass('visible').addClass('hidden');
+            }
+        }
+    };
+
+    // ページが読み込まれた時点でアニメーションを実行
+    scrollAnimationFunc();
+
+    // スクロール時にもアニメーションを実行
+    $(window).on('scroll', scrollAnimationFunc);
+});
+
