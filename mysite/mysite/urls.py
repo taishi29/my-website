9d8caed1cb1ec670a5805ac15642ca8a86451ad1
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-10-18 17:18:21 +0900

    urlを「変更

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from pages import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls') ), # include関数は、引数に指定したモジュールを呼び出す。'pages'フォルダ内のurls.pyが読み込まれ、pages/より後のアドレスがそこから割り当てられる。
+    path('', views.index, name='home'),  # トップページにアクセスした際に pages.views.index を表示
 ]
