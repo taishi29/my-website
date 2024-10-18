cfac72404a852f754e09f299af58508fb9cca500
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-10-18 17:51:56 +0900

    urls.pyにDEBUGの状況に合わせて、staticフォルダの関係を示さなければならない？

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from pages import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls') ), # include関数は、引数に指定したモジュールを呼び出す。'pages'フォルダ内のurls.pyが読み込まれ、pages/より後のアドレスがそこから割り当てられる。
     path('', views.index, name='home'),  # トップページにアクセスした際に pages.views.index を表示
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
