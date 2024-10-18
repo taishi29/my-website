a6831690726cdd681242f16f8fdb57cae3e55442
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-10-18 17:22:11 +0900

    urlを「変更

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from pages import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls') ), # include関数は、引数に指定したモジュールを呼び出す。'pages'フォルダ内のurls.pyが読み込まれ、pages/より後のアドレスがそこから割り当てられる。
