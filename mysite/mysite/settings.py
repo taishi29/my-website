363950fcf1aa98435d727be9f96c8df9d55d9f09
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-10-18 17:25:36 +0900

    staticファイルをsettings.pyに名義

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -109,6 +109,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
