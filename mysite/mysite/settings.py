1835178dba0a6c94183370361d47949adb76a324
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-10-18 18:21:28 +0900

    修正

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -35,7 +35,7 @@ DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 MEDIA_URL = 'pages/media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'pages/static'
+STATIC_URL = 'pages/static/pages'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
