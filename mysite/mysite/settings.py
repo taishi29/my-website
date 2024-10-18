909ba850de573af01ee733bb71ffaec5b987e401
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-10-18 18:19:55 +0900

    修正

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,10 +32,10 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 # 静的ファイルとメディアファイルの設定
-MEDIA_URL = '/media/'
+MEDIA_URL = 'pages/media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/static/'
+STATIC_URL = 'pages/static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
